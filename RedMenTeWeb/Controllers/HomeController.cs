a80f070c67a284cf40da7c7f070eb33e0bf88563
lito-gut <kathygutierrez42@gmail.com>
2025-04-03 13:33:13 -0600

    Cambios varios
    login, registro y recuperacion contraseña, layout

--- RedMenTeWeb/Controllers/HomeController.cs
+++ RedMenTeWeb/Controllers/HomeController.cs
@@ -8,11 +8,26 @@ namespace RedMenTeWeb.Controllers
 {
     public class HomeController : Controller
     {
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        public ActionResult RegistrarCuenta()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        [HttpGet]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,10 +35,18 @@ namespace RedMenTeWeb.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [HttpGet]
+
+        public ActionResult RecuperarContrasenna()
+        {
             return View();
         }
     }
