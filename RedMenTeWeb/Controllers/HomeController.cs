fb212a35f692f99ad997677edb5c16dcd07e6690
Oscar <orodriguez70465@ufide.ac.cr>
2025-04-10 16:08:27 -0600

    Funcionalidades de usuario

---
+++ RedMenTeWeb/Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedMenTeWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RegistrarCuenta()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Proyectos()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RecordatorioProgreso()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+
+        public ActionResult RecuperarContrasenna()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Calendario()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Foro()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DetalleForo()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CrearPublicacion()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult MisCertificados()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DetalleCertificados()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Seguridad()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Estadisticas()
+        {
+            return View();
+        }
+    }
+}
